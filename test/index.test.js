54262b4807f78fadc4a35d725bc042c10d96506f
Elvis Onobo <elvis.onobo@gmail.com>
2023-05-09 17:36:46 +0100

    test: mock the Watchman class

---
+++ test/index.test.js
@@ -0,0 +1,16 @@
+import Watchman from "../lib";
+
+jest.mock("../lib")
+
+it('Should post an error to the error endpoint', () => {
+    const watchman = new Watchman("wm_hbhsahjdbhcvhuiqwkubcqkjbcn", "dev");
+    watchman.capture({
+        "message": "Cannot read property 'foo' of undefined",
+        "name": "TypeError",
+        "stack": "TypeError: Cannot read property 'foo' of undefined\n    at myFunction (/path/to/file.js:12:23)\n    at /path/to/otherfile.js:23:7",
+        "fileName": "/path/to/file.js",
+        "lineNumber": 12,
+        "columnNumber": 23
+    })
+    expect(watchman.capture).toHaveBeenCalledTimes(1);
+});
